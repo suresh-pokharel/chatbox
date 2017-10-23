08b64cd3a6ea6cec4b7dc4bc8dad09258110331f
Suresh Pokharel <suresh.wrc@gmail.com>
2017-10-23 15:52:13 +0545

    User's ID at the side of box added

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 	var user_id = Math.floor((Math.random() * 10000) + 1);
+	socket.emit('my id',user_id);					// emit the user's id 
+	
 	socket.on('chat message', function(msg){
 		var data = {
 			user_id : user_id,
@@ -17,7 +19,11 @@ io.on('connection', function(socket){
   });
 
 	socket.on('connection success', function(msg){
-    	io.emit('connection success', 'user-' + user_id + ' ' + msg);
+		var data = {
+			user_id : user_id,
+			msg: msg
+		};
+    	io.emit('connection success', data);
   });
 
 	socket.on('disconnect', function(msg){
